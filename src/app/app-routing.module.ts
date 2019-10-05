66ff829f0c049962a11ecd68cd4de0edec5866ac
Sankalp100 <sankalpgour123@gmail.com>
2019-10-05 15:06:47 +0530

    v3.01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { FavListComponent } from './components/fav-list/fav-list.component';
 import { TableComponent } from './components/table/table.component';
 
 const routes: Routes = [
-  {path:'/', component: TableComponent},
+  {path:'', component: TableComponent},
   {path:'favourite', component: FavListComponent},
-  {path: '', redirectTo:'/', pathMatch: 'full'}
+  {path: '', redirectTo:'', pathMatch: 'full'}
 ];
 
 @NgModule({
